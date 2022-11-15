5bfd4d3d71eab0b1893f224799810e0b784827f9
kjh42447 <kjh42447@gmail.com>
2022-11-15 17:52:03 +0900

    ✨feat: add UserController postUser, patchUser

--- OneCoin/Server/src/main/java/OneCoin/Server/user/controller/UserController.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/user/controller/UserController.java
@@ -1,4 +1,52 @@
 package OneCoin.Server.user.controller;
 
+import OneCoin.Server.dto.SingleResponseDto;
+import OneCoin.Server.user.dto.UserDto;
+import OneCoin.Server.user.entity.User;
+import OneCoin.Server.user.mapper.UserMapper;
+import OneCoin.Server.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+
+@RestController
+@RequestMapping("/members")
+@Validated
+@Slf4j
 public class UserController {
+    private UserService userService;
+    private UserMapper userMapper;
+
+    public UserController(UserService userService, UserMapper userMapper) {
+        this.userService = userService;
+        this.userMapper = userMapper;
+    }
+
+    @PostMapping
+    public ResponseEntity postUser(@Valid @RequestBody UserDto.Post requestBody) {
+        User user = userService.createUser(userMapper.userPostToUser(requestBody));
+
+        return new ResponseEntity<>(
+                new SingleResponseDto<>(userMapper.userToUserResponse(user)), HttpStatus.CREATED
+        );
+    }
+
+    @PatchMapping("/{user-id}")
+    public ResponseEntity patchUser(
+            @PathVariable("user-id") @Positive long userId,
+            @Valid @RequestBody UserDto.Patch requestBody) {
+        User user = userMapper.userPatchToUser(requestBody);
+        user.setUserId(userId);
+
+        User updatedUser = userService.updateUser(user);
+
+        return new ResponseEntity<>(
+                new SingleResponseDto<>(userMapper.userToUserResponse(updatedUser)), HttpStatus.CREATED
+        );
+    }
 }
--- OneCoin/Server/src/main/java/OneCoin/Server/user/repository/UserRepository.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/user/repository/UserRepository.java
@@ -1,4 +1,15 @@
 package OneCoin.Server.user.repository;
 
-public interface UserRepository {
+
+import OneCoin.Server.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    /** 이메일로 유저 검색 */
+    Optional<User> findByEmail(String email);
+
+    /** 이메일로 계정 존재 여부 검색 */
+    Boolean existsByEmail(String email);
 }
