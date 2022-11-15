062020f3214723687c62f0fc82ff77b0f9330986
kjh42447 <kjh42447@gmail.com>
2022-11-15 14:08:01 +0900

    ✨feat: add build.gradle dependency.mapstruct

--- OneCoin/Server/src/main/java/OneCoin/Server/user/dto/UserDto.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/user/dto/UserDto.java
@@ -1,4 +1,59 @@
 package OneCoin.Server.user.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 public class UserDto {
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Post {
+        @NotBlank
+        private String displayName;
+
+        @NotBlank
+        @Email
+        private String email;
+
+        // 정규표현식 제약조건 추가 필요
+        @NotBlank
+        private String password;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Patch {
+        @NotBlank
+        private String displayName;
+
+        @NotBlank
+        @Email
+        private String email;
+
+        // 정규표현식 제약조건 추가 필요
+        @NotBlank
+        private String password;
+
+        private long balance;
+    }
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Response {
+        private long id;
+
+        private String displayName;
+
+        private String email;
+    }
+
 }
--- OneCoin/Server/src/main/java/OneCoin/Server/user/mapper/UserMapper.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/user/mapper/UserMapper.java
@@ -1,4 +1,7 @@
 package OneCoin.Server.user.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
 }
