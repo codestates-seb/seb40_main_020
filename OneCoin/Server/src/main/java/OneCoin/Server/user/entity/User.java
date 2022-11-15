4529ba7108d8490b3755de3e7345c6c27a1d4e30
kjh42447 <kjh42447@gmail.com>
2022-11-15 11:27:08 +0900

    ✨feat: User, Role, Platform

--- OneCoin/Server/src/main/java/OneCoin/Server/user/entity/User.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/user/entity/User.java
@@ -1,4 +1,42 @@
 package OneCoin.Server.user.entity;
 
-public class User {
+import OneCoin.Server.audit.Auditable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class User extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(length = 30, nullable = false)
+    private String displayName;
+
+    @Column(length = 50, nullable = false, updatable = false, unique = false)
+    private String email;
+
+    @Column(length = 30, nullable = false)
+    private String password;
+
+    // 추후 Attribute Converter 사용 고려
+    @Column
+    @Enumerated(EnumType.STRING)
+    private Platform platform;
+
+    @Column(nullable = false)
+    private long balance;
+
+    // 추후 Attribute Converter 사용하거나 테이블 분리도 고려
+    @Column
+    @Enumerated(EnumType.STRING)
+    private Role userRole;
 }
