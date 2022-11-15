af2d8c87c3615934645b533ffc51e4bea2d4aca0
kjh42447 <kjh42447@gmail.com>
2022-11-15 17:52:03 +0900

    ✨feat: add UserController postUser, patchUser

--- OneCoin/Server/src/main/java/OneCoin/Server/user/mapper/UserMapper.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/user/mapper/UserMapper.java
@@ -1,7 +1,51 @@
 package OneCoin.Server.user.mapper;
 
+import OneCoin.Server.user.dto.UserDto;
+import OneCoin.Server.user.entity.Platform;
+import OneCoin.Server.user.entity.Role;
+import OneCoin.Server.user.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
+    default User userPostToUser(UserDto.Post requestBody) {
+        User user = new User();
+
+        user.setDisplayName(requestBody.getDisplayName());
+        user.setEmail(requestBody.getEmail());
+        user.setPassword(requestBody.getPassword());
+        user.setPlatform(Platform.ORIGIN);
+        user.setBalance(0L);
+        user.setUserRole(Role.ROLE_USER);
+
+        return user;
+    }
+
+    default User userPatchToUser(UserDto.Patch requestBody) {
+        User user = new User();
+
+        user.setDisplayName(requestBody.getDisplayName());
+        user.setEmail(requestBody.getEmail());
+        user.setPassword(requestBody.getPassword());
+        user.setPlatform(Platform.ORIGIN);
+        user.setBalance(0L);
+        user.setUserRole(Role.ROLE_USER);
+
+        return user;
+    }
+
+    default UserDto.Response userToUserResponse(User user) {
+        UserDto.Response response = new UserDto.Response();
+
+        response.setId(user.getUserId());
+        response.setEmail(user.getEmail());
+        response.setDisplayName(user.getDisplayName());
+
+        return response;
+    }
+
+    List<UserDto.Response> usersToUserResponses(List<User> users);
 }
--- OneCoin/Server/src/main/java/OneCoin/Server/user/service/UserService.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/user/service/UserService.java
@@ -1,4 +1,58 @@
 package OneCoin.Server.user.service;
 
+import OneCoin.Server.user.entity.User;
+import OneCoin.Server.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    /** 유저 생성 */
+    @Transactional
+    public User createUser(User user) {
+        // 계정 존재 여부 조회
+        if (!hasAccount(user.getEmail())) {
+            // 계정 생성
+            return userRepository.save(user);
+        }
+
+        // 기존 계정 반환
+        return userRepository.findByEmail(user.getEmail()).orElseThrow();
+    }
+
+    /**
+     * <pre>
+     *     회원 정보 변경
+     *     displayName, email, password, balance 만 변경 가능
+     * </pre>
+     */
+    @Transactional
+    public User updateUser(User user) {
+        User findUser = userRepository.findById(user.getUserId()).orElseThrow();
+
+        findUser.setDisplayName(user.getDisplayName());
+        findUser.setEmail(user.getEmail());
+        findUser.setPassword(user.getPassword());
+        findUser.setBalance(user.getBalance());
+
+        return userRepository.save(findUser);
+    }
+
+    /**
+     * <pre>
+     *     계정 존재 여부 조회
+     *     이메일, 플랫폼이 동일한 계정이 있으면 true
+     * </pre>
+     */
+    @Transactional
+    public Boolean hasAccount(String email) {
+        return userRepository.existsByEmail(email);
+    }
 }
