c13b514cfa1269bcdd97db84195fd32a2a3a01e2
NAM GIBEOM <rlqja0523@naver.com>
2022-11-16 19:35:18 +0900

    ✨ feat: 매수/매도 주문 저장

---
+++ OneCoin/Server/src/main/java/OneCoin/Server/order/service/OrderService.java
@@ -0,0 +1,18 @@
+package OneCoin.Server.order.service;
+
+import OneCoin.Server.order.entity.RedisOrder;
+import OneCoin.Server.order.repository.RedisOrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final RedisOrderRepository redisOrderRepository;
+
+    public RedisOrder createOrder(RedisOrder redisOrder) {
+        // TODO User mapping
+        return redisOrderRepository.save(redisOrder);
+    }
+}
