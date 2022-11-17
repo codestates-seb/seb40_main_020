06d73acb18d2542a416b422f64c0288c725939b1
NAM GIBEOM <rlqja0523@naver.com>
2022-11-17 16:23:08 +0900

    ✨ feat: findAll 메서드 List 타입으로 반환하도록 overriding

--- OneCoin/Server/src/main/java/OneCoin/Server/order/repository/RedisOrderRepository.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/order/repository/RedisOrderRepository.java
@@ -3,5 +3,10 @@ package OneCoin.Server.order.repository;
 import OneCoin.Server.order.entity.RedisOrder;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface RedisOrderRepository extends CrudRepository<RedisOrder, Long> {
+
+    @Override
+    List<RedisOrder> findAll();
 }
