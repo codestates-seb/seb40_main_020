b2d0ed083cae1cfb4bb53f33dff16c1fb727d922
NAM GIBEOM <rlqja0523@naver.com>
2022-11-17 19:52:10 +0900

    ♻ refactor: String 타입으로 변환해서 값 대입

--- OneCoin/Server/src/main/java/OneCoin/Server/order/mapper/OrderMapper.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/order/mapper/OrderMapper.java
@@ -4,10 +4,24 @@ import OneCoin.Server.order.dto.RedisOrderDto;
 import OneCoin.Server.order.entity.RedisOrder;
 import org.mapstruct.Mapper;
 
+import java.math.BigDecimal;
+
 @Mapper(componentModel = "spring")
 public interface OrderMapper {
 
-    RedisOrder redisPostDtoToRedisOrder(RedisOrderDto.Post redisPostDto);
+    default RedisOrder redisPostDtoToRedisOrder(RedisOrderDto.Post redisPostDto) {
+        if (redisPostDto == null) {
+            return null;
+        }
+
+        return RedisOrder.builder()
+                        .limit(new BigDecimal(String.valueOf(redisPostDto.getLimit())))
+                        .market(new BigDecimal(String.valueOf(redisPostDto.getMarket())))
+                        .stopLimit(new BigDecimal(String.valueOf(redisPostDto.getStopLimit())))
+                        .amount(new BigDecimal(String.valueOf(redisPostDto.getAmount())))
+                        .askOrBid(redisPostDto.isAskOrBid())
+                        .build();
+    }
 
     RedisOrderDto.PostResponse redisOrderToRedisPostResponse(RedisOrder redisOrder);
 }
