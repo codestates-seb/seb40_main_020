60a6ef85237c2764c9b9c78ca550428c4acd7b8d
broden-horizon <rlghd1698@naver.com>
2022-11-17 17:48:37 +0900

    🧪test: ChatRoom Repository 기능 테스트
    - 이름으로 조회

--- OneCoin/Server/src/test/java/OneCoin/Server/ChatRoomRepositoryTest.java
+++ OneCoin/Server/src/test/java/OneCoin/Server/ChatRoomRepositoryTest.java
@@ -2,17 +2,21 @@ package OneCoin.Server;
 
 import OneCoin.Server.chat.chatRoom.entity.ChatRoom;
 import OneCoin.Server.chat.chatRoom.repository.ChatRoomRepository;
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
+@Transactional
 public class ChatRoomRepositoryTest {
     @Autowired
     private ChatRoomRepository repository;
@@ -24,6 +28,7 @@ public class ChatRoomRepositoryTest {
         ChatRoom chatRoom3 = ChatRoom.builder().name("도지").nation("us").build();
         repository.save(chatRoom1);
         repository.save(chatRoom2);
+        repository.save(chatRoom3);
     }
 
     @Test
@@ -35,5 +40,16 @@ public class ChatRoomRepositoryTest {
         //then
         assertThat(rooms.size())
                 .isEqualTo(2);
+    }
+
+    @Test
+    void 이름조회() {
+        //given
+        String name = "도지";
+        //when
+        Optional<ChatRoom> room = repository.findByName(name);
+        //then
+        assertThat(room.isPresent())
+                .isTrue();
     }
 }
