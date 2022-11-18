33dc33e873d63f3764119005197770c7cebf953b
munchking98 <blood89@nate.com>
2022-11-18 09:42:15 +0900

    ✨feat :  Asidebar(coinlist) 컴포넌트

---
+++ OneCoin/Client/src/components/Aside/index.tsx
@@ -0,0 +1,127 @@
+import React, { useState, useEffect } from 'react';
+import { AsideComponent } from './style';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { coinSocketState, coinDataState } from '../../store';
+import { useRecoilValue, useRecoilState } from 'recoil';
+
+interface Props {
+	symbolHandler: (item: { coin: string; code: string; symbol: string }) => void;
+}
+
+function Aside({ symbolHandler }: Props) {
+	const mainTitleMenu = ['코인', '랭킹'];
+	const [title, setTitle] = useState('코인');
+	const titleClickHandler = (item: string) => setTitle(item);
+	const subTitleMenu = ['코인명', '현재가', '전일대비'];
+
+	const [coinlist, setCoinlist] = useRecoilState(coinDataState);
+	const newcoinlist = [...coinlist];
+	const socket = useRecoilValue(coinSocketState);
+	const coinCodes = coinlist.map((v) => v.code);
+
+	function socketAPI(codes: { type: string; codes: string[] }[]) {
+		const data = [{ ticket: 'test' }, ...codes];
+		socket.addEventListener('open', function (event) {
+			socket.send(JSON.stringify(data));
+		});
+		socket.addEventListener('message', function (event) {
+			event.data.text().then((res: string) => {
+				const data = JSON.parse(res);
+				const idx = coinCodes.indexOf(data.code);
+				console.log(data);
+				if (data.type === 'ticker') newcoinlist[idx].ticker = data;
+				if (data.type === 'orderbook') newcoinlist[idx].orderbook = data;
+			});
+		});
+		socket.addEventListener('error', (event) => {
+			console.log(event);
+		});
+	}
+
+	useEffect(() => {
+		const codes = [
+			{ type: 'ticker', codes: coinCodes },
+			{ type: 'orderbook', codes: coinCodes },
+		];
+		socketAPI(codes);
+
+		const coinPriceInterval = setInterval(() => {
+			setCoinlist(newcoinlist);
+		}, 1000);
+		return () => clearInterval(coinPriceInterval);
+	}, []);
+
+	return (
+		<AsideComponent>
+			<div className="aside-title">
+				{mainTitleMenu.map((v, i) => (
+					<div
+						key={i}
+						onClick={() => titleClickHandler(v)}
+						className={title === v ? 'select' : ''}
+					>
+						{v}
+					</div>
+				))}
+			</div>
+			{title === '코인' ? (
+				<div>
+					{/* title이 코인 일때 */}
+					<div className="coin-search">
+						<input type="text" placeholder="코인명 검색" />
+						<div className="serach-icon">
+							<AiOutlineSearch />
+						</div>
+					</div>
+					<table className="sub-title">
+						<thead>
+							<tr>
+								{subTitleMenu.map((v, i) => (
+									<td key={i}>{v}</td>
+								))}
+							</tr>
+						</thead>
+						<tbody>
+							{coinlist &&
+								coinlist.map((v, i) => (
+									<tr
+										key={i}
+										onClick={() =>
+											symbolHandler({
+												coin: v.coin,
+												code: v.code,
+												symbol: v.symbol,
+											})
+										}
+									>
+										<td>{v.coin}</td>
+										<td className={'today-range'}>
+											{v?.ticker?.trade_price
+												? v.ticker.trade_price.toLocaleString()
+												: 0}
+										</td>
+										<tr className={'today-range'}>
+											<td>
+												{v?.ticker?.signed_change_rate
+													? `${(v.ticker.signed_change_rate * 100).toFixed(2)}%`
+													: '0%'}
+											</td>
+											<td>
+												{v?.ticker?.signed_change_price
+													? v.ticker.signed_change_price.toLocaleString()
+													: 0}
+											</td>
+										</tr>
+									</tr>
+								))}
+						</tbody>
+					</table>
+				</div>
+			) : (
+				<div>랭킹</div>
+			)}
+		</AsideComponent>
+	);
+}
+
+export default Aside;
---
+++ OneCoin/Client/src/utills/types/index.ts
@@ -0,0 +1,73 @@
+export interface TickerType {
+	acc_ask_volume?: number;
+	acc_bid_volume?: number;
+	acc_trade_price?: number;
+	acc_trade_price_24h?: number;
+	acc_trade_volume?: number;
+	acc_trade_volume_24h?: number;
+	ask_bid?: string;
+	change?: string;
+	change_price?: number;
+	change_rate?: number;
+	code?: string;
+	delisting_date?: null;
+	high_price?: number;
+	highest_52_week_date?: string;
+	highest_52_week_price?: number;
+	is_trading_suspended?: boolean;
+	low_price?: number;
+	lowest_52_week_date?: string;
+	lowest_52_week_price?: number;
+	market_state?: string;
+	market_warning?: string;
+	opening_price?: number;
+	prev_closing_price?: number;
+	signed_change_price?: number;
+	signed_change_rate?: number;
+	stream_type?: string;
+	timestamp?: number;
+	trade_date?: string;
+	trade_price?: number;
+	trade_time?: string;
+	trade_timestamp?: number;
+	trade_volume?: number;
+	type?: string;
+}
+
+export interface OrderbookType {
+	code?: string;
+	orderbook_units?: {
+		ask_price: number;
+		bid_price: number;
+		ask_size: number;
+		bid_size: number;
+	}[];
+	stream_type?: string;
+	timestamp?: number;
+	total_ask_size?: number;
+	total_bid_size?: number;
+	type?: string;
+}
+
+export interface CoinDataType {
+	coin: string;
+	symbol: string;
+	code: string;
+	ticker?: TickerType;
+	orderbook?: OrderbookType;
+}
+
+export interface coinAsiderData {
+	coin: string;
+	symbol: string;
+	code: string;
+	tradePrice?: number;
+	change?: string;
+	signedChangePrice?: number;
+	signedChangeRate?: number;
+}
+// enum
+export interface OrderType {
+	price: number;
+	size: number;
+}
