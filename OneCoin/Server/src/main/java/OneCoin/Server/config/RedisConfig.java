e17a32805ce8f37c8af9102ef65b3870e6e1eb2c
broden-horizon <rlghd1698@naver.com>
2022-11-18 15:42:48 +0900

    ✨feat: 채팅에 쓰일 redis 관련 설정

--- OneCoin/Server/src/main/java/OneCoin/Server/config/RedisConfig.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/config/RedisConfig.java
@@ -5,7 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.HashMapperProvider;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -17,6 +22,35 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private int port;
 
+    @Bean // Redis Channel(Topic)에서 메시지를 받고 주입된 리스너에게 비동기적으로 displat하는 컨테이너
+    public RedisMessageListenerContainer redisMessageListenerContainer(
+            RedisConnectionFactory connectionFactory,
+            MessageListenerAdapter listenerAdapter,
+            ChannelTopic channelTopic
+    ) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(listenerAdapter, channelTopic);
+        return container;
+    }
+    @Bean
+    public MessageListenerAdapter listenerAdapter(RedisSubscriber subscriber) {
+        // subscriber자리는 delegator. 실제 서비스 로직이 담긴다.
+        // 디폴트로 onMessage 메서드가 작동한다.
+        return new MessageListenerAdapter(subscriber,"onMessage");
+    }
+    @Bean//redis의 데이터를 조작하는 두 가지 방법 중 하나. redis는 byte코드만 저장하기때문에 직렬화하는클래스가 필요.
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
+        return redisTemplate;
+    }
+    @Bean //예는 redis 전용 topic
+    public ChannelTopic channelTopic() {
+        return new ChannelTopic("chatroom");
+    }
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(host, port);
