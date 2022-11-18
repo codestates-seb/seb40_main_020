e7e8047cd8f2c55047d961f21825474f88e4553f
broden-horizon <rlghd1698@naver.com>
2022-11-19 00:46:52 +0900

    ✨feat: WebSocket Controller 구현

--- OneCoin/Server/src/main/java/OneCoin/Server/chat/chatMessage/controller/ChatController.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/chat/chatMessage/controller/ChatController.java
@@ -1,33 +1,30 @@
 package OneCoin.Server.chat.chatMessage.controller;
 
-import OneCoin.Server.chat.chatMessage.dto.ChatMessageDto;
-import OneCoin.Server.chat.chatRoom.service.ChatRoomService;
-import OneCoin.Server.chat.constant.MessageType;
+import OneCoin.Server.chat.chatMessage.dto.ChatRequestDto;
+import OneCoin.Server.chat.chatMessage.dto.ChatResponseDto;
+import OneCoin.Server.chat.chatMessage.entity.ChatMessage;
+import OneCoin.Server.chat.chatMessage.mapper.ChatMapper;
+import OneCoin.Server.chat.chatMessage.service.ChatService;
+import OneCoin.Server.chat.redis.RedisPublisher;
 import lombok.AllArgsConstructor;
+import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-
-import javax.validation.Valid;
 
 @Controller
 @AllArgsConstructor
 public class ChatController {
-    private final SimpMessagingTemplate template;
-    private final ChatRoomService chatRoomService;
+    private final RedisPublisher redisPublisher;
+    private final ChatService chatService;
+    private final ChatMapper chatMapper;
 
     @MessageMapping("/rooms")//여기에는 @Payload가 생략되어 있음. body를 객체로 mapping시켜줌
-    public void message(@Valid ChatMessageDto message) {
-        verifyRoom(message.getRoomId());
-        if(message.getType().equals(MessageType.ENTER)) {
-            message.setMessage(message.getSender()+"님이 입장하였습니다");
-        }
-        template.convertAndSend("/topic/rooms/" + message.getRoomId(),message);
-    }
-
-    private void verifyRoom(long roomId) {
-        chatRoomService.findChatRoom(roomId);
+    public void message(ChatRequestDto requestMessage) {
+        ChatMessage convertedChatMessage = chatMapper.requestDtoToChatMessage(requestMessage);
+        ChatMessage chatMessage = chatService.deligate(requestMessage.getType(), convertedChatMessage);
+        ChatResponseDto chatResponseDto = chatMapper.chatMessageToResponseDto(chatMessage);
+        ChannelTopic topic = chatService.getTopic(chatResponseDto.getChatRoomId());
+        redisPublisher.publish(topic, chatResponseDto);
     }
 }
 
