d8e3ca5aafa3ae584a6b7f27ea34fcbf59317f60
MINDA01 <ekdud4639@gmail.com>
2022-11-18 22:54:26 +0900

    ✨feat :  거래내역 상수 추가

--- OneCoin/Client/src/utills/constants/investments.ts
+++ OneCoin/Client/src/utills/constants/investments.ts
@@ -1,3 +1,4 @@
+// balance 보유자산
 export const INVERSTMENTS_LIST = [
 	{ name: '보유자산', link: '/investments/balance' },
 	{ name: '거래내역', link: '/investments/history' },
@@ -41,3 +42,56 @@ export const BALANCE_TBODY = {
 		totalPages: 10,
 	},
 };
+
+// history 거래내역
+export const HISTORY_THEAD = [
+	'체결시간',
+	'코인',
+	'마켓',
+	'종류',
+	'거래수량',
+	'거래단가',
+	'거래금액',
+	'수수료',
+	'정산금액',
+	'주문시간',
+];
+
+export const HISTORY_TBODY = {
+	data: [
+		{
+			id: 1,
+			execution_time: '2022. 09.09. 07:58', // 체결시간
+			coin: '비트토렌트', // 코인
+			market: '-', // 마켓
+			type: '입금', //종류
+			quantity: '0.00000417 BTT', // 거래수량
+			unit_price: '0 KRW', // 거래단가
+			transaction_amount: '0 KRW', // 거래금액
+			premium: '0 APENFT', // 수수료
+			settlement_amount: '0.0000000001 APENFT', // 정산금액
+			order_time: '-', // 주문시간
+		},
+	],
+	pageInfo: {
+		page: 1,
+		size: 10,
+		totalElements: 100,
+		totalPages: 10,
+	},
+};
+
+export const HISTORY_PERIOD = [
+	{ title: '1주일', value: '' },
+	{ title: '1개월', value: '1' },
+	{ title: '3개월', value: '3' },
+	{ title: '6개월', value: '6' },
+];
+
+export const HISTORY_TYPE = [
+	{ title: '전체', value: '' },
+	{ title: '매수', value: 'buy' },
+	{ title: '매도', value: 'sell' },
+	{ title: '입금', value: 'deposit' },
+	{ title: '스왑', value: 'swap' },
+];
