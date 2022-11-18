7f6c1609eb3408be6ea58cb41cdc848afa0f3cbe
MINDA01 <ekdud4639@gmail.com>
2022-11-18 22:52:10 +0900

    ✨feat :  거래내역페이지 HistoryHeader 컴포넌트 추가

---
+++ OneCoin/Client/src/pages/History/components/HistoryHeader/style.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+import { spanTitle } from '../ButtonList/style';
+
+export const Wrapper = styled.div`
+	display: flex;
+	width: 100%;
+	margin: 50px 0 30px;
+`;
+
+export const SearchBox = styled.div`
+	flex: 1;
+	span {
+		${spanTitle}
+	}
+	> div {
+		position: relative;
+		width: 100%;
+		input {
+			width: 100%;
+			border: 1px solid var(--borderColor);
+			padding: 10px 30px 10px 10px;
+			box-sizing: border-box;
+		}
+		.icon {
+			position: absolute;
+			top: 50%;
+			right: 0;
+			transform: translate(-50%, -50%);
+			cursor: pointer;
+		}
+	}
+`;
