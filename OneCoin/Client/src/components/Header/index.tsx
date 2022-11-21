fd153436bbdb714337eebd86f5c8f4f5dcff36fa
munchking98 <blood89@nate.com>
2022-11-21 09:51:42 +0900

    ✨feat :  Header 컴포넌트

--- OneCoin/Client/src/components/Header/index.tsx
+++ OneCoin/Client/src/components/Header/index.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
 import { HeaderComponent } from './style';
-import logo from '../../assets/images/open.png';
+import { useNavigate } from 'react-router-dom';
+import logo from '../../assets/images/one.png';
+import { HEADER_LIST } from '../../utills/constants/investments';
 
 function Header() {
+	const navigate = useNavigate();
+
+	const navClickHandler = (path: string) => navigate(path);
+	const isLogin = false;
 	return (
 		<HeaderComponent>
-			<img src={logo} />
+			<div className="content">
+				<div className="nav-page nav">
+					<img src={logo} onClick={() => navigate('/')} />
+					{HEADER_LIST.page.map((v, i) => (
+						<div key={i} onClick={() => navClickHandler(v.path)}>
+							{v.name}
+						</div>
+					))}
+				</div>
+				<div className="nav-member nav">
+					{isLogin
+						? HEADER_LIST.login.map((v, i) => (
+								<div key={i} onClick={() => navClickHandler(v.path)}>
+									{v.name}
+								</div>
+						  ))
+						: HEADER_LIST.logout.map((v, i) => (
+								<div key={i} onClick={() => navClickHandler(v.path)}>
+									{v.name}
+								</div>
+						  ))}
+				</div>
+			</div>
 		</HeaderComponent>
 	);
 }
