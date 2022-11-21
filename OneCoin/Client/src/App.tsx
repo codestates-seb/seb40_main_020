1591b0f2349d03c5402e7b81269a02b5f2baf283
korean-davinci <wo55ow231@gmail.com>
2022-11-21 09:17:16 +0900

    ✨  메인 페이지 템플릿 추가

--- OneCoin/Client/src/App.tsx
+++ OneCoin/Client/src/App.tsx
@@ -4,7 +4,7 @@ import SignUp from 'pages/SignUp/SignUp';
 import Login from 'pages/Login/Login';
 import FindPassword from 'pages/FindPassword/FindPassword';
 import ResetPassword from 'pages/FindPassword/ResetPassword';
-
+import Main from 'pages/Main/Main';
 const Balance = lazy(() => import('./pages/Balance'));
 const Exchange = lazy(() => import('./pages/Exchange'));
 
@@ -14,7 +14,7 @@ function App() {
 			<Routes>
 				<Route
 					path="/"
-					element={<Suspense fallback={<>...</>}>{<div>메인</div>}</Suspense>}
+					element={<Suspense fallback={<>...</>}>{<Main />}</Suspense>}
 				/>
 				<Route
 					path="/exchange"
---
+++ OneCoin/Client/src/components/Chat/style.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const ChatComponent = styled.div`
+	width: 100%;
+	height: 100%;
+	box-sizing: border-box;
+`;
