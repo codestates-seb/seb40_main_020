a486854e29d19fa1e61c5e3a5b43b6584579c612
munchking98 <blood89@nate.com>
2022-11-21 09:54:38 +0900

    ✨feat :  Footer 컴포넌트

--- OneCoin/Client/src/utills/constants/investments.ts
+++ OneCoin/Client/src/utills/constants/investments.ts
@@ -27,6 +27,22 @@ export const HEADER_LIST = {
 	login: [{ name: '마이페이지', path: '/mypage' }],
 };
 
+export const FOOTER_LIST = {
+	repository: 'https://github.com/codestates-seb/seb40_main_020',
+	copyright: '1Coin © 2022 All rights reserved.',
+	fe: [
+		{ name: '서재홍', repository: 'https://github.com/jaehongg' },
+		{ name: '김민상', repository: 'https://github.com/MINDA01' },
+		{ name: '민다영', repository: 'https://github.com/minsang98' },
+		{ name: '유현규', repository: 'https://github.com/korean-davinci' },
+	],
+	be: [
+		{ name: '김기홍', repository: 'https://github.com/broaden-horizon' },
+		{ name: '김지환', repository: 'https://github.com/kjh42447' },
+		{ name: '남기범', repository: 'https://github.com/GIVEN53' },
+	],
+};
+
 export const BALANCE_THEAD = [
 	'보유자산',
 	'보유수량',
