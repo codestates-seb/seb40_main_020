b3ef25aa1448dd945d1aff473d9a60f62509328c
munchking98 <blood89@nate.com>
2022-11-21 09:51:42 +0900

    ✨feat :  Header 컴포넌트

--- OneCoin/Client/src/utills/constants/investments.ts
+++ OneCoin/Client/src/utills/constants/investments.ts
@@ -14,6 +14,20 @@ export const BALANCE_INFO_LIST = [
 	{ name: '총 평가수익률', currency: '%' },
 ];
 
+export const HEADER_LIST = {
+	page: [
+		{ name: '거래소', path: '/exchange' },
+		{ name: '입금', path: '/balancese' },
+		{ name: '투자내역', path: '/investments/balance' },
+		{ name: '스왑', path: '/swap' },
+	],
+	logout: [
+		{ name: '로그인', path: '/login' },
+		{ name: '회원가입', path: '/signup' },
+	],
+	login: [{ name: '마이페이지', path: '/mypage' }],
+};
+
 export const BALANCE_THEAD = [
 	'보유자산',
 	'보유수량',
