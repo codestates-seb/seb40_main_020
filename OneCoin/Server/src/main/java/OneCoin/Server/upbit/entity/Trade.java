d523419048f9ec51719c84a420834197f6b0f2f7
NAM GIBEOM <rlqja0523@naver.com>
2022-11-22 10:52:51 +0900

    ✨ feat: getter, setter 추가, 타입 변경

--- OneCoin/Server/src/main/java/OneCoin/Server/upbit/entity/Trade.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/upbit/entity/Trade.java
@@ -1,10 +1,15 @@
 package OneCoin.Server.upbit.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Trade { // 체결
     private String code; // 마켓 코드
-    private double tradePrice; // 체결 가격
-    private double tradeVolume; // 체결량
-    private String askBid; // 매수/매도 구분 ASK(매도) BID(매수)
+    private String tradePrice; // 체결 가격 (원래 더블)
+    private String tradeVolume; // 체결량 (원래 더블)
+    private String orderType; // 매수/매도 구분 ASK(매도) BID(매수)
     private String tadeDate; // 체결 일자
-    private String tradeTime; // 체결 시각
+    private String tradeTime; // 체결 시각 UTC기준, +9시간해야 한국 시간
 }
