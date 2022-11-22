a9ee45b5bb14a8840324d1c0e95c570406d3d145
MINDA01 <ekdud4639@gmail.com>
2022-11-22 17:08:35 +0900

    ✨feat :  mypage 퍼블 작업

--- OneCoin/Client/src/pages/MyPage/index.tsx
+++ OneCoin/Client/src/pages/MyPage/index.tsx
@@ -7,18 +7,27 @@ const MyPage = () => {
 	return (
 		<Layout isLeftSidebar={false}>
 			<Wrapper>
-				<ul>
+				<ul className="side-menu">
 					<li>회원정보 수정</li>
 					<li>회원탈퇴</li>
 				</ul>
-				<div>
-					<div>
+				<div className="content-wrapper">
+					<div className="profile-box">
 						<img src="" alt="profile 이미지" />
 						<ul>
 							<li>닉네임: ???</li>
 							<li>이메일: 1234@gmail.com</li>
 						</ul>
 					</div>
+					<div className="input-box">
+						<span>닉네임 변경</span>
+						<input type="text" placeholder="닉네임" />
+					</div>
+					<div className="input-box">
+						<span>비밀번호 변경</span>
+						<input type="password" placeholder="비밀번호" />
+					</div>
+					<button>수정하기</button>
 				</div>
 			</Wrapper>
 		</Layout>
