ea401b5a09e88ecf91b9e0d46cb779671cba0f48
Gibeom Nam <rlqja0523@naver.com>
2022-11-23 20:36:10 +0900

    ♻ refactor: dto를 바로 사용하므로 삭제

--- OneCoin/Server/src/main/java/OneCoin/Server/upbit/repository/OrderBookRepository.java
+++
@@ -1,7 +0,0 @@
-package OneCoin.Server.upbit.repository;
-
-import OneCoin.Server.upbit.entity.OrderBook;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderBookRepository extends CrudRepository<OrderBook, String> {
-}
