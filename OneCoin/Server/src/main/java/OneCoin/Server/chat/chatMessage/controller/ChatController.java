0b56a3008e91132948ca92de765c3c02a4b8e408
broden-horizon <rlghd1698@naver.com>
2022-11-24 01:57:29 +0900

    📦chore: logger 추가

--- OneCoin/Server/src/main/java/OneCoin/Server/chat/chatMessage/controller/ChatController.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/chat/chatMessage/controller/ChatController.java
@@ -6,6 +6,7 @@ import OneCoin.Server.chat.chatMessage.entity.ChatMessage;
 import OneCoin.Server.chat.chatMessage.mapper.ChatMapper;
 import OneCoin.Server.chat.chatMessage.service.ChatService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 @AllArgsConstructor
+@Slf4j
 public class ChatController {
     private final ChatService chatService;
     private final ChatMapper chatMapper;
@@ -21,9 +23,12 @@ public class ChatController {
 
     @MessageMapping("/rooms")//여기에는 @Payload가 생략되어 있음. body를 객체로 mapping시켜줌
     public void message(ChatRequestDto requestMessage) {
+        log.info("message received: {}", requestMessage);
         ChatMessage convertedChatMessage = chatMapper.requestDtoToChatMessage(requestMessage);
         ChatMessage chatMessage = chatService.delegate(requestMessage.getType(), convertedChatMessage);
         ChatResponseDto chatResponseDto = chatMapper.chatMessageToResponseDto(chatMessage);
+        log.info("[CONTROLLER] message is ready for sending");
+        log.info("[MESSAGE] {}", chatResponseDto);
         redisTemplate.convertAndSend(channelTopic.getTopic(), chatResponseDto);
     }
 }
