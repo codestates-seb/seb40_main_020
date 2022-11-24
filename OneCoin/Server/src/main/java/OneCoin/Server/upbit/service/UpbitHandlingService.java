d1ceba99eadd37b90dd6dea1582a9d019cd95a91
Gibeom Nam <rlqja0523@naver.com>
2022-11-24 09:19:21 +0900

    ✨ feat: 파싱 완료된 데이터 저장

--- OneCoin/Server/src/main/java/OneCoin/Server/upbit/service/UpbitHandlingService.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/upbit/service/UpbitHandlingService.java
@@ -10,17 +10,22 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class UpbitHandlingService {
     private final ObjectMapper objectMapper;
     private final TradingService tradingService;
     private final OrderBookDtoMapper mapper;
+    private final RedisTemplate<String, Object> redisTemplate;
 
     private String prevClosingPrice;
 
@@ -30,6 +35,7 @@ public class UpbitHandlingService {
 
         if (orderType.equals(SiseType.TICKER.getType())) {
             TickerDto tickerDto = objectMapper.readValue(jsonNode.toString(), TickerDto.class);
+            saveTicker(tickerDto);
             prevClosingPrice = tickerDto.getPrevClosingPrice();
             Trade trade = objectMapper.readValue(jsonNode.toString(), Trade.class);
 //            tradingService.completeOrders(trade);
@@ -38,7 +44,17 @@ public class UpbitHandlingService {
             OrderBookDto orderBookDto = objectMapper.readValue(jsonNode.toString(), OrderBookDto.class);
             List<UnitInfo> unitInfos = Arrays.asList(objectMapper.readValue(jsonNode.get("orderbook_units").toString(), UnitInfo[].class));
             orderBookDto = mapper.unitInfoToOrderBookDto(orderBookDto, unitInfos, prevClosingPrice);
-            // 미완성
+            saveOrderBook(orderBookDto);
         }
+    }
+
+    private void saveTicker(TickerDto tickerDto) {
+        HashOperations<String, String, TickerDto> operations = redisTemplate.opsForHash();
+        operations.put(SiseType.TICKER.getType(), tickerDto.getCode(), tickerDto);
+    }
+
+    private void saveOrderBook(OrderBookDto orderBookDto) {
+        HashOperations<String, String, OrderBookDto> operations = redisTemplate.opsForHash();
+        operations.put(SiseType.ORDER_BOOK.getType(), orderBookDto.getCode(), orderBookDto);
     }
 }
