2baaf70559e03ee88a6829b483b4ef9bd1bc1ce3
kjh42447 <kjh42447@gmail.com>
2022-11-24 16:28:48 +0900

    merge: USer.java

--- OneCoin/Server/src/main/java/OneCoin/Server/balance/entity/Balance.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/balance/entity/Balance.java
@@ -1,6 +1,6 @@
 package OneCoin.Server.balance.entity;
 
-import OneCoin.Server.audit.CreatedOnlyAuditable;
+import OneCoin.Server.audit.Auditable;
 import OneCoin.Server.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,12 +14,12 @@ import javax.persistence.*;
 @Getter
 @Setter
 @Entity
-public class Balance extends CreatedOnlyAuditable {
+public class Balance extends Auditable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long balanceId;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "balance_user_id")
     private User user;
 
--- OneCoin/Server/src/main/java/OneCoin/Server/balance/mapper/BalanceMapper.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/balance/mapper/BalanceMapper.java
@@ -1,8 +1,15 @@
 package OneCoin.Server.balance.mapper;
 
+import OneCoin.Server.balance.dto.BalanceDto;
+import OneCoin.Server.balance.entity.Balance;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface BalanceMapper {
+    Balance balancePatchToBalance(BalanceDto.Patch requestBody);
+    BalanceDto.Response balanceToBalanceResponse(Balance balance);
+    List<BalanceDto.Response> balancesToBalanceResponses(List<Balance> balances);
 }
