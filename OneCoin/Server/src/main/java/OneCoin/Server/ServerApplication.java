3686bebf3bc112b3db5b87b9b9a1f5ba7f63c0c2
broden-horizon <rlghd1698@naver.com>
2022-11-25 10:55:08 +0900

    ✨feat: 실시간으로 채팅방에 몇 명있는지 message 보내는 기능 구현

--- OneCoin/Server/src/main/java/OneCoin/Server/ServerApplication.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/ServerApplication.java
@@ -3,9 +3,11 @@ package OneCoin.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableScheduling
 public class ServerApplication {
 
     public static void main(String[] args) {
