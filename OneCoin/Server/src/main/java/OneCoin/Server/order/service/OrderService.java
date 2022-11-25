df3fad3eb043a8d9a0ae3f92167d40032faf959c
Gibeom Nam <rlqja0523@naver.com>
2022-11-25 20:10:53 +0900

    ✨ feat: balance만 연결하면 완성

--- OneCoin/Server/src/main/java/OneCoin/Server/order/service/OrderService.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/order/service/OrderService.java
@@ -1,11 +1,12 @@
 package OneCoin.Server.order.service;
 
-import OneCoin.Server.coin.entity.Coin;
 import OneCoin.Server.coin.service.CoinService;
 import OneCoin.Server.config.auth.utils.LoggedInUserInfoUtils;
 import OneCoin.Server.exception.BusinessLogicException;
 import OneCoin.Server.exception.ExceptionCode;
 import OneCoin.Server.order.entity.Order;
+import OneCoin.Server.order.entity.Wallet;
+import OneCoin.Server.order.entity.enums.TransactionType;
 import OneCoin.Server.order.repository.OrderRepository;
 import OneCoin.Server.user.entity.User;
 import lombok.RequiredArgsConstructor;
@@ -23,40 +24,37 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final CoinService coinService;
+    private final WalletService walletService;
     private final LoggedInUserInfoUtils loggedInUserInfoUtils;
 
     public Order createOrder(Order order, String code) {
-        // TODO User mapping
         User user = loggedInUserInfoUtils.extractUser();
-        Coin coin = coinService.findVerifiedCoin(code);
-        if (order.getOrderType().equals("ASK")) { // 매도 // TODO ask, bid enum으로
-            checkUserHasCoin(coin);
-            checkUserCoinAmount(1L, order.getAmount());
+        long userId = user.getUserId();
+        coinService.findVerifiedCoin(code);
+
+        if (order.getOrderType().equals(TransactionType.ASK.getType())) { // 매도
+            Wallet wallet = verifyCoinInMyWallet(userId, code);
+            checkUserCoinAmount(wallet, order.getAmount());
         }
-        if (order.getOrderType().equals("BID")) { // 매수
-            // 예외 확인
+        if (order.getOrderType().equals(TransactionType.BID.getType())) { // 매수
+            BigDecimal price = getMyOrderPrice(order);
+            updateUserBalance(user, price, order.getAmount());
         }
         order.setUserId(user.getUserId());
         order.setCode(code);
         return orderRepository.save(order);
     }
 
-    // TODO 코인, 가격, 똑같은 주문이 있으면 덮어쓰기
-    private long checkUserHasCoin(Coin coin) { // user가 해당 코인을 가지고 있는지 확인 - User로 리턴
-        // TODO jwt User확인, wallet에 코인 있는지 확인
-        long userId = 1L;
-        // walletService ..
-        // if(Wallet.)
-        if (1 == 0) {
+    private Wallet verifyCoinInMyWallet(long userId, String code) {
+        Wallet wallet = walletService.findMyWallet(userId, code);
+        if (wallet == null) {
             throw new BusinessLogicException(ExceptionCode.HAVE_NO_COIN);
         }
-        return userId;
+        return wallet;
     }
 
-    private void checkUserCoinAmount(long userId, BigDecimal amount) { // user가 보유한 코인의 양보다 작거나 같은지 확인 - user의 지갑
-        // TODO parameter User로 변경
-        // BigDecimal myAmount = use
-        BigDecimal myAmount = BigDecimal.valueOf(1000);
+    private void checkUserCoinAmount(Wallet wallet, BigDecimal amount) {
+        BigDecimal myAmount = wallet.getAmount();
         int comparison = myAmount.compareTo(amount);
 
         if (comparison < 0) {
@@ -65,35 +63,48 @@ public class OrderService {
     }
 
     private void updateUserBalance(User user, BigDecimal price, BigDecimal amount) {
-        BigDecimal balance = new BigDecimal(String.valueOf(100000000)); // user.getBalance();
+        BigDecimal balance = new BigDecimal(String.valueOf(100000000)); // TODO user.getBalance();
         BigDecimal totalBidPrice = price.multiply(amount);
-        checkEnoughBalance(balance, totalBidPrice);
+        verifyEnoughBalance(balance, totalBidPrice);
 
         // TODO user balance 업데이트 - user쪽 서비스 로직으로 진행
     }
 
-    private void checkEnoughBalance(BigDecimal balance, BigDecimal totalBidPrice) {
+    private void verifyEnoughBalance(BigDecimal balance, BigDecimal totalBidPrice) {
         int comparison = balance.compareTo(totalBidPrice);
         if (comparison < 0) {
             throw new BusinessLogicException(ExceptionCode.NOT_ENOUGH_BALANCE);
         }
     }
 
+    private BigDecimal getMyOrderPrice(Order order) {
+        BigDecimal price = null;
+        BigDecimal zero = BigDecimal.ZERO;
+        if (order.getLimit().compareTo(zero) != 0) {
+            price = order.getLimit();
+        } else if (order.getMarket().compareTo(zero) != 0) {
+            price = order.getMarket();
+        } else if (order.getStopLimit().compareTo(zero) != 0) {
+            price = order.getStopLimit();
+        }
+        return price;
+    }
+
     public void cancelOrder(long orderId) {
-        Order order = verifiedOrder(orderId);
-        isSameUser(order);
+        Order order = findVerifiedOrder(orderId);
+        long userId = loggedInUserInfoUtils.extractUserId();
+        verifySameUser(order, userId);
+        // TODO balance 다시 돌려주기
 
         orderRepository.delete(order);
     }
 
-    private Order verifiedOrder(long orderId) {
+    private Order findVerifiedOrder(long orderId) {
         Optional<Order> optionalRedisOrder = orderRepository.findById(orderId);
         return optionalRedisOrder.orElseThrow(() -> new BusinessLogicException(ExceptionCode.NO_EXISTS_ORDER));
     }
 
-    private void isSameUser(Order order) {
-        // TODO redisOrder에 매핑된 UserId와 취소를 요청하는 UserId가 같은지 확인
-        long userId = 1L;
+    private void verifySameUser(Order order, long userId) {
         if (order.getUserId() != userId) {
             throw new BusinessLogicException(ExceptionCode.NOT_YOUR_ORDER);
         }
@@ -101,8 +112,11 @@ public class OrderService {
 
     @Transactional(readOnly = true)
     public List<Order> findOrders(String code) {
-        // TODO jwt User의 것만 찾기
-        long userId = 1L;
-        return orderRepository.findAllByUserIdAndCode(userId, code);
+        long userId = loggedInUserInfoUtils.extractUserId();
+        List<Order> myOrders = orderRepository.findAllByUserIdAndCode(userId, code);
+        if (myOrders.isEmpty()) {
+            throw new BusinessLogicException(ExceptionCode.NO_EXISTS_ORDER);
+        }
+        return myOrders;
     }
 }
