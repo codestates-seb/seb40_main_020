ec8ea522ca4fd2bcbf5b32f15e214371026f4aa4
Gibeom Nam <rlqja0523@naver.com>
2022-11-26 14:44:18 +0900

    ♻ refactor: 메서드 분리

--- OneCoin/Server/src/main/java/OneCoin/Server/upbit/service/UpbitHandlingService.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/upbit/service/UpbitHandlingService.java
@@ -4,7 +4,6 @@ import OneCoin.Server.upbit.dto.orderbook.OrderBookDto;
 import OneCoin.Server.upbit.dto.ticker.TickerDto;
 import OneCoin.Server.upbit.entity.Trade;
 import OneCoin.Server.upbit.entity.UnitInfo;
-import OneCoin.Server.upbit.entity.enums.CoinList;
 import OneCoin.Server.upbit.entity.enums.SiseType;
 import OneCoin.Server.upbit.mapper.OrderBookDtoMapper;
 import OneCoin.Server.upbit.repository.OrderBookRepository;
@@ -13,16 +12,12 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import org.springframework.data.redis.core.HashOperations;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class UpbitHandlingService {
     private final ObjectMapper objectMapper;
@@ -33,22 +28,32 @@ public class UpbitHandlingService {
 
     private String prevClosingPrice;
 
-    @SneakyThrows
     public void parsing(JsonNode jsonNode) {
-        String orderType = jsonNode.get("type").asText();
+        String type = jsonNode.get("type").asText();
 
-        if (orderType.equals(SiseType.TICKER.getType())) { // 현재가 정보
-            TickerDto tickerDto = objectMapper.readValue(jsonNode.toString(), TickerDto.class);
-            tickerRepository.saveTicker(tickerDto);
-            prevClosingPrice = tickerDto.getPrevClosingPrice();
-            Trade trade = objectMapper.readValue(jsonNode.toString(), Trade.class);
-//            tradingService.completeOrders(trade);
+        if (type.equals(SiseType.TICKER.getType())) { // 현재가 정보
+            handleTicker(jsonNode);
         }
-        if (orderType.equals(SiseType.ORDER_BOOK.getType())) { // 호가 정보
-            OrderBookDto orderBookDto = objectMapper.readValue(jsonNode.toString(), OrderBookDto.class);
-            List<UnitInfo> unitInfos = Arrays.asList(objectMapper.readValue(jsonNode.get("orderbook_units").toString(), UnitInfo[].class));
-            orderBookDto = mapper.unitInfoToOrderBookDto(orderBookDto, unitInfos, prevClosingPrice);
-            orderBookRepository.saveOrderBook(orderBookDto);
+        if (type.equals(SiseType.ORDER_BOOK.getType())) { // 호가 정보
+            handleOrderBook(jsonNode);
         }
+    }
+
+    @SneakyThrows
+    private void handleTicker(JsonNode jsonNode) {
+        TickerDto tickerDto = objectMapper.readValue(jsonNode.toString(), TickerDto.class);
+        prevClosingPrice = tickerDto.getPrevClosingPrice();
+        tickerRepository.saveTicker(tickerDto);
+
+        Trade trade = objectMapper.readValue(jsonNode.toString(), Trade.class);
+        tradingService.completeOrders(trade);
+    }
+
+    @SneakyThrows
+    private void handleOrderBook(JsonNode jsonNode) {
+        OrderBookDto orderBookDto = objectMapper.readValue(jsonNode.toString(), OrderBookDto.class);
+        List<UnitInfo> unitInfos = Arrays.asList(objectMapper.readValue(jsonNode.get("orderbook_units").toString(), UnitInfo[].class));
+        orderBookDto = mapper.unitInfoToOrderBookDto(orderBookDto, unitInfos, prevClosingPrice);
+        orderBookRepository.saveOrderBook(orderBookDto);
     }
 }
