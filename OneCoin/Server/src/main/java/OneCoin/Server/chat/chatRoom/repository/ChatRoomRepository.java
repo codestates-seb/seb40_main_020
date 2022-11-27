915e267b1c77457a8f56df6a209f096f89ea3ab8
broden-horizon <rlghd1698@naver.com>
2022-11-28 04:37:33 +0900

    ♻️refactor: Crud -> Redis Template으로 변경

--- OneCoin/Server/src/main/java/OneCoin/Server/chat/chatRoom/repository/ChatRoomRepository.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/chat/chatRoom/repository/ChatRoomRepository.java
@@ -1,94 +1,39 @@
 package OneCoin.Server.chat.chatRoom.repository;
 
-import OneCoin.Server.chat.chatRoom.entity.ChatRoom;
-import OneCoin.Server.exception.BusinessLogicException;
-import OneCoin.Server.exception.ExceptionCode;
+import OneCoin.Server.chat.chatRoom.utils.ChatRoomUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.SetOperations;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
 public class ChatRoomRepository {
-    private final RedisTemplate<String, Object> redisTemplate;
-    private SetOperations<String, Object> operations;
-    private final String KEY_FOR_CHAT_ROOMS = "chatRooms";
-    private final String PREFIX_OF_KEY = "ChatRoom";
-    private final String SUFFIX_OF_KEY = "Session";
+    private final ChatRoomUtils chatRoomUtils;
+    private final RedisTemplate<String, String> redisTemplate;
+    private SetOperations<String, String> setOperations;// <KEY_FOR_CHAT_ROOMS, ChatRoomIdKey>
 
 
     @PostConstruct
     private void init() {
-        operations = redisTemplate.opsForSet();
+        setOperations = redisTemplate.opsForSet();
     }
 
-    public void saveSessionIdToChatRoom(Integer chatRoomId, String sessionId) {
-        boolean isValid = doesChatRoomExist(chatRoomId);
-        if (!isValid) {
-            makeChatRoom(chatRoomId);
-        }
-        operations.add(getChatRoomKey(chatRoomId), sessionId);
+    public void create(Integer chatRoomId) {
+        setOperations.add(chatRoomUtils.KEY_FOR_CHAT_ROOMS, chatRoomUtils.getKey(chatRoomId));
     }
 
-    public long getNumberOfSessionConnectedToChatRoom(Integer chatRoomId) {
-        verifyChatRoom(chatRoomId);
-        return operations.size(getChatRoomKey(chatRoomId));
-    }
-    // return 0 when nothing is removed;
-    public long deleteSessionFromChatRoom(Integer chatRoomId, String sessionId) {
-        verifyChatRoom(chatRoomId);
-        return operations.remove(getChatRoomKey(chatRoomId), sessionId);
+    public Set<String> findAll() {
+        return setOperations.members(chatRoomUtils.KEY_FOR_CHAT_ROOMS);
     }
 
-    public List<ChatRoom> findChatRooms() {
-       Set<Object> chatRooms = operations.members(KEY_FOR_CHAT_ROOMS);
-       return chatRooms.stream().map(c -> {
-           Integer chatRoomId = parseChatRoomId((String) c);
-           long numberOfSessions = getNumberOfSessionConnectedToChatRoom(chatRoomId);
-           return ChatRoom.builder()
-                   .chatRoomId(chatRoomId)
-                   .numberOfChatters(numberOfSessions)
-                   .build();
-       }).collect(Collectors.toList());
+    public boolean contains(Integer chatRoomId) {
+        return setOperations.isMember(chatRoomUtils.KEY_FOR_CHAT_ROOMS, chatRoomUtils.getKey(chatRoomId));
     }
-
-    public void deleteChatRoom(Integer chatRoomId) {
-        verifyChatRoom(chatRoomId);
-        redisTemplate.delete(getChatRoomKey(chatRoomId));
-        operations.remove(KEY_FOR_CHAT_ROOMS, getChatRoomKey(chatRoomId));
-    }
-
-    public void makeChatRoom(Integer chatRoomId) {
-        Set<Object> chatRooms = operations.members(KEY_FOR_CHAT_ROOMS);
-        int numberOfChatRooms = chatRooms.size();
-        if(numberOfChatRooms + 1 != chatRoomId) {
-            throw new BusinessLogicException(ExceptionCode.INVALID_CHAT_ROOM_ID);
-        }
-        operations.add(KEY_FOR_CHAT_ROOMS, getChatRoomKey(chatRoomId));
-    }
-
-    public boolean doesChatRoomExist(Integer chatRoomId) {
-        return operations.isMember(KEY_FOR_CHAT_ROOMS, getChatRoomKey(chatRoomId));
-    }
-
-    private String getChatRoomKey(Integer chatRoomId) {
-        return PREFIX_OF_KEY + String.valueOf(chatRoomId) + SUFFIX_OF_KEY;
-    }
-    private Integer parseChatRoomId(String key) {
-        String chatRoomIdAsString = key.replace(PREFIX_OF_KEY, "");
-        chatRoomIdAsString = chatRoomIdAsString.replace(SUFFIX_OF_KEY, "");
-        return Integer.parseInt(chatRoomIdAsString);
-    }
-    public void verifyChatRoom(Integer chatRoomId) {
-        boolean isValid = doesChatRoomExist(chatRoomId);
-        if(!isValid) {
-            throw new BusinessLogicException(ExceptionCode.INVALID_CHAT_ROOM_ID);
-        }
+    public void delete(Integer chatRoomId) {
+        setOperations.remove(chatRoomUtils.KEY_FOR_CHAT_ROOMS, chatRoomUtils.getKey(chatRoomId));
     }
 }
---
+++ OneCoin/Server/src/test/java/OneCoin/Server/chat/chatRoom/repository/ChatRoomRepositoryTest.java
@@ -0,0 +1,64 @@
+package OneCoin.Server.chat.chatRoom.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+public class ChatRoomRepositoryTest {
+    @Autowired
+    private ChatRoomRepository chatRoomRepository;
+
+    @Test
+    void createAndFindAllTest() {
+        //givn
+        Integer chatRoomId1 = 1;
+        Integer chatRoomId2 = 2;
+        //when
+        chatRoomRepository.create(chatRoomId1);
+        chatRoomRepository.create(chatRoomId2);
+        //then
+        Set<String> chatRooms = chatRoomRepository.findAll();
+        assertThat(chatRooms.size())
+                .isEqualTo(2);
+
+    }
+
+    @Test
+    void containsTest_True() {
+        //given
+        int chatRoomId = 1;
+        //when
+        boolean isExist = chatRoomRepository.contains(chatRoomId);
+        //then
+        assertThat(isExist)
+                .isTrue();
+    }
+
+    @Test
+    void containsTest_False() {
+        //given
+        int chatRoomId = 10;
+        //when
+        boolean isExist = chatRoomRepository.contains(chatRoomId);
+        //then
+        assertThat(isExist)
+                .isFalse();
+    }
+
+    @Test
+    void deleteChatRoomTest() {
+        //given
+        int chatRoomIdToDelete = 1;
+        //when
+        chatRoomRepository.delete(chatRoomIdToDelete);
+        //then
+        Set<String> chatRooms = chatRoomRepository.findAll();
+        assertThat(chatRooms.size())
+                .isEqualTo(1);
+    }
+}
