e494ba8b62457541af89b6780d2b3c4e94e9cda1
broden-horizon <rlghd1698@naver.com>
2022-11-28 04:43:22 +0900

    ♻️refactor: 매서드 명 및 클래스 명 변경

--- OneCoin/Server/src/main/java/OneCoin/Server/config/WebSocketConfig.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/config/WebSocketConfig.java
@@ -1,9 +1,8 @@
 package OneCoin.Server.config;
 
-import OneCoin.Server.chat.chatRoom.service.ChatRoomInMemoryService;
-import OneCoin.Server.chat.intercepter.MessageInterceptor;
 import OneCoin.Server.chat.chatMessage.publisher.RedisPublisher;
-import OneCoin.Server.config.auth.utils.AuthorizationUtilsForWebSocket;
+import OneCoin.Server.chat.chatRoom.service.ChatRoomService;
+import OneCoin.Server.chat.intercepter.MessageInterceptor;
 import OneCoin.Server.config.auth.utils.LoggedInUserInfoUtilsForWebSocket;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
@@ -22,8 +21,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Order(Ordered.HIGHEST_PRECEDENCE + 99)
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     private final LoggedInUserInfoUtilsForWebSocket loggedInUserInfoUtilsForWebSocket;
-    private final AuthorizationUtilsForWebSocket authorizationUtilsForWebSocket;
-    private final ChatRoomInMemoryService chatRoomInMemoryService;
+    private final ChatRoomService chatRoomService;
     private final RedisPublisher redisPublisher;
 
     @Override
@@ -48,7 +46,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(new MessageInterceptor(loggedInUserInfoUtilsForWebSocket
-                , authorizationUtilsForWebSocket, chatRoomInMemoryService, redisPublisher));
+        registration.interceptors(new MessageInterceptor(
+                loggedInUserInfoUtilsForWebSocket, chatRoomService, redisPublisher));
     }
+
+
 }
