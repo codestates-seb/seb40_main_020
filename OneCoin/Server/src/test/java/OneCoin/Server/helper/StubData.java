69c496e90e4cb4fe05a0fbfc4b2f6d7f95435465
Gibeom Nam <rlqja0523@naver.com>
2022-11-29 18:28:46 +0900

    ✨ feat: api stub data 추가

--- OneCoin/Server/src/test/java/OneCoin/Server/helper/StubData.java
+++ OneCoin/Server/src/test/java/OneCoin/Server/helper/StubData.java
@@ -69,4 +69,54 @@ public class StubData {
             return coin;
         }
     }
+
+    public static class MockUpbitAPI {
+        public static String getJsonTicker() {
+            return "{" +
+                    "\"type\":\"ticker\"," +
+                    "\"code\":\"KRW-BTC\"," +
+                    "\"high_price\":22587000," +
+                    "\"low_price\":22155000," +
+                    "\"trade_price\":22525000," +
+                    "\"prev_closing_price\":2.2339E7," +
+                    "\"change\":\"RISE\"," +
+                    "\"change_price\":186000.0," +
+                    "\"change_rate\":0.0083262456," +
+                    "\"ask_bid\":\"BID\"," +
+                    "\"trade_volume\":2.2237E-4," +
+                    "\"trade_time\":\"071622\"," +
+                    "\"timestamp\":1669706182202," +
+                    "\"acc_trade_price_24h\":6.390681659003425E10," +
+                    "\"acc_trade_volume_24h\":2854.74052566" +
+                    "}";
+        }
+
+        public static String getJsonOrderBook() {
+            return "{" +
+                    "\"type\":\"orderbook\"," +
+                    "\"code\":\"KRW-BTC\"," +
+                    "\"total_ask_size\":5.365745690000001," +
+                    "\"total_bid_size\":13.740592939999999," +
+                    "\"orderbook_units\":" +
+                    "[" +
+                    "{\"ask_price\":2.2525E7,\"bid_price\":2.2521E7,\"ask_size\":7.83E-5,\"bid_size\":0.18899245}," +
+                    "{\"ask_price\":2.2529E7,\"bid_price\":2.252E7,\"ask_size\":0.4,\"bid_size\":0.0017718}," +
+                    "{\"ask_price\":2.253E7,\"bid_price\":2.2511E7,\"ask_size\":0.48605748,\"bid_size\":0.6392}," +
+                    "{\"ask_price\":2.2532E7,\"bid_price\":2.251E7,\"ask_size\":0.31829486,\"bid_size\":0.6}," +
+                    "{\"ask_price\":2.2533E7,\"bid_price\":2.2509E7,\"ask_size\":0.03062177,\"bid_size\":0.58319307}," +
+                    "{\"ask_price\":2.2534E7,\"bid_price\":2.2507E7,\"ask_size\":4.5431E-4,\"bid_size\":0.00223437}," +
+                    "{\"ask_price\":2.2535E7,\"bid_price\":2.2506E7,\"ask_size\":0.5291849,\"bid_size\":0.01780993}," +
+                    "{\"ask_price\":2.2537E7,\"bid_price\":2.2505E7,\"ask_size\":0.36774815,\"bid_size\":0.05282096}," +
+                    "{\"ask_price\":2.2538E7,\"bid_price\":2.2504E7,\"ask_size\":0.375,\"bid_size\":0.17519573}," +
+                    "{\"ask_price\":2.2539E7,\"bid_price\":2.2503E7,\"ask_size\":2.9253E-4,\"bid_size\":0.13094987}," +
+                    "{\"ask_price\":2.254E7,\"bid_price\":2.2502E7,\"ask_size\":0.04893041,\"bid_size\":0.18022975}," +
+                    "{\"ask_price\":2.2541E7,\"bid_price\":2.25E7,\"ask_size\":0.60760042,\"bid_size\":1.2637169}," +
+                    "{\"ask_price\":2.2542E7,\"bid_price\":2.2499E7,\"ask_size\":0.03392823,\"bid_size\":0.01782062}," +
+                    "{\"ask_price\":2.2543E7,\"bid_price\":2.2498E7,\"ask_size\":1.84489573,\"bid_size\":2.32341126}," +
+                    "{\"ask_price\":2.2544E7,\"bid_price\":2.2497E7,\"ask_size\":0.3226586,\"bid_size\":7.56324623}" +
+                    "]" +
+                    "}";
+
+        }
+    }
 }
