b89c48e03e7b9fab047ea82d763e66086d3e4916
broden-horizon <rlghd1698@naver.com>
2022-12-01 14:28:19 +0900

    ♻️refactor: 클래스의 책임 분리
    1. Interceptor, RedisPublisher
    As-Was: RedisPublisher에서 서비스로직 (입장 / 퇴장 메시지 작성)과 메시지 전송 로직이 혼재
    AS-Is: publish 하는 책임만을 남기고 나니 redisTemplate.convertAndSend() 메서드 한 줄만 남아 아에 해당 클래스를 제거했음.
    IntegerCeptor는 일종의 컨트롤러임. 사용자의 요청을 받아서 적절한 서비스 로직에 위임하고 다시 전달하는 책임. 이에 따라 리팩터링 진행함.
    
    2. AuthUtil, AuthService
    As-Was 인증 작업을 AuthUtil에서 진행하고 있었음.
    As-Is AuthService를 만들어 책임을 분리함.

--- OneCoin/Server/src/main/java/OneCoin/Server/chat/controller/MessageInterceptor.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/chat/controller/MessageInterceptor.java
@@ -1,9 +1,13 @@
-package OneCoin.Server.chat.intercepter;
+package OneCoin.Server.chat.controller;
 
-import OneCoin.Server.chat.publisher.RedisPublisher;
-import OneCoin.Server.chat.service.ChatRoomService;
-import OneCoin.Server.chat.vo.UserInfoInChatRoom;
 import OneCoin.Server.chat.constant.MessageType;
+import OneCoin.Server.chat.dto.ChatResponseDto;
+import OneCoin.Server.chat.entity.ChatMessage;
+import OneCoin.Server.chat.mapper.ChatMapper;
+import OneCoin.Server.chat.service.ChatRoomService;
+import OneCoin.Server.chat.service.ChatService;
+import OneCoin.Server.config.webSocketAuth.WebSocketAuthService;
+import OneCoin.Server.chat.repository.vo.UserInfoInChatRoom;
 import OneCoin.Server.config.auth.utils.UserUtilsForWebSocket;
 import OneCoin.Server.exception.BusinessLogicException;
 import OneCoin.Server.exception.ExceptionCode;
@@ -11,13 +15,14 @@ import OneCoin.Server.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 @Slf4j
@@ -25,8 +30,11 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 public class MessageInterceptor implements ChannelInterceptor {
     private final UserUtilsForWebSocket userUtilsForWebSocket;
     private final ChatRoomService chatRoomService;
-    private final RedisPublisher redisPublisher;
-    private final String AUTHORIZATION = "Authorization";
+    private final WebSocketAuthService webSocketAuthService;
+    private final ChatService chatService;
+    private final ChatMapper chatMapper;
+    private final RedisTemplate<Object, Object> redisTemplate;
+    private final ChannelTopic channelTopic;
 
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
@@ -35,11 +43,13 @@ public class MessageInterceptor implements ChannelInterceptor {
         String sessionId = accessor.getSessionId();
         if (StompCommand.CONNECT.equals(command)) {
             log.info("[CONNECT] start {}", sessionId);
-            authenticate(accessor);
+            webSocketAuthService.authenticate(accessor);
             log.info("[CONNECT] complete {}", sessionId);
         } else if (StompCommand.SUBSCRIBE.equals(command)) {
             log.info("[SUBSCRIBE] start {}", sessionId);
-            registerUserAndSendEnterMessage(accessor);
+            Integer chatRoomId = parseRoomIdFromHeader(accessor); //채팅 구독이 아니면 null반환
+            User user = userUtilsForWebSocket.extractUser(accessor.getUser()); //비회원은 null을 반환
+            registerUserAndSendEnterMessage(sessionId, chatRoomId, user);
             log.info("[SUBSCRIBE] complete {}", sessionId);
         } else if (StompCommand.UNSUBSCRIBE.equals(command)) {
             log.info("[UNSUBSCRIBE] start {}", sessionId);
@@ -61,35 +71,25 @@ public class MessageInterceptor implements ChannelInterceptor {
         log.info("[ABNORMAL DISCONNECT] unregister complete : user {}", sessionId);
     }
 
-    private void authenticate(StompHeaderAccessor accessor) {
-        String accessToken = accessor.getFirstNativeHeader(AUTHORIZATION);
-        if (accessToken == null || accessToken.equals("") || accessToken.equals("null")) return;
-        Authentication authentication = userUtilsForWebSocket.authenticate(accessToken);
-        accessor.setUser(authentication);
-    }
 
     //구독시에는 유저 방 정보에 유저가 몇 명 있고, 누가 있는지만 저장
-    private void registerUserAndSendEnterMessage(StompHeaderAccessor accessor) {
-        Integer chatRoomId = parseRoomIdFromHeader(accessor); //채팅 구독이 아니면 null반환
-        if (chatRoomId == null) return;
-        User user = userUtilsForWebSocket.extractUser(accessor.getUser()); //비회원은 null을 반환
-        String sessionId = accessor.getSessionId();
-        if (user == null) { // 비회원이라면
-            chatRoomService.saveUserToChatRoom(chatRoomId, sessionId);
-            log.info("register user complete : unloggedInUser {} -> room {}", sessionId, chatRoomId);
-            return;
-        }
+    private void registerUserAndSendEnterMessage(String sessionId, Integer chatRoomId, User user) {
+        if (chatRoomId == null) return; //채팅 서비스가 아닌 경우
         chatRoomService.saveUserToChatRoom(chatRoomId, sessionId, user);
-        redisPublisher.publishEnterOrLeaveMessage(MessageType.ENTER, chatRoomId, user);
-        log.info("register user complete : user {} -> room {}", sessionId, chatRoomId);
+        if (user != null) { //로그인한 유저인 경우
+            ChatMessage messageToUse = chatService.makeEnterOrLeaveChatMessage(MessageType.ENTER, chatRoomId, user);
+            ChatResponseDto chatResponseDto = chatMapper.chatMessageToResponseDto(messageToUse);
+            redisTemplate.convertAndSend(channelTopic.getTopic(), chatResponseDto);
+        }
     }
 
     private void unregisterUserAndSendLeaveMessage(String sessionId) {
         UserInfoInChatRoom user = chatRoomService.deleteUserFromChatRoom(sessionId);
-        if (user.getUser() != null) {
-            redisPublisher.publishEnterOrLeaveMessage(MessageType.LEAVE, user.getChatRoomId(), user.getUser());
+        if (user.getUser() != null) { //로그인한 유저인 경우
+            ChatMessage messageToUse = chatService.makeEnterOrLeaveChatMessage(MessageType.LEAVE, user.getChatRoomId(), user.getUser());
+            ChatResponseDto chatResponseDto = chatMapper.chatMessageToResponseDto(messageToUse);
+            redisTemplate.convertAndSend(channelTopic.getTopic(), chatResponseDto);
         }
-        log.info("unregister user complete : unloggedInUser {}", sessionId);
     }
 
     private Integer parseRoomIdFromHeader(StompHeaderAccessor accessor) {
