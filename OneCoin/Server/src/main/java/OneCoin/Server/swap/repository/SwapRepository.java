2f130dd3f5f53eba7600f9e3c3fb1c8707764fd0
kjh42447 <kjh42447@gmail.com>
2022-12-01 15:51:29 +0900

    ✨feat: init SwapRepository

--- OneCoin/Server/src/main/java/OneCoin/Server/swap/repository/SwapRepository.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/swap/repository/SwapRepository.java
@@ -1,4 +1,7 @@
 package OneCoin.Server.swap.repository;
 
-public interface SwapRepository {
+import OneCoin.Server.swap.entity.Swap;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SwapRepository extends JpaRepository<Swap, Long> {
 }
