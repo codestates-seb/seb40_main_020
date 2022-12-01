44fc3d669c531a9bd7d1bd7a3754166c67ceeb66
kjh42447 <kjh42447@gmail.com>
2022-12-01 23:09:59 +0900

    ✨feat: init calculateExchangeRate

--- OneCoin/Server/src/main/java/OneCoin/Server/swap/mapper/SwapMapper.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/swap/mapper/SwapMapper.java
@@ -3,13 +3,19 @@ package OneCoin.Server.swap.mapper;
 import OneCoin.Server.swap.dto.SwapDto;
 import OneCoin.Server.swap.entity.Swap;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface SwapMapper {
+    @Mapping(source = "givenCoin", target = "givenCoin.code")
+    @Mapping(source = "takenCoin", target = "takenCoin.code")
     Swap swapPostToSwap(SwapDto.Post requestBody);
+
+    @Mapping(source = "givenCoin.code", target = "givenCoin")
+    @Mapping(source = "takenCoin.code", target = "takenCoin")
     SwapDto.Response swapToSwapResponseDto(Swap swap);
     List<SwapDto.Response> swapsToSwapResponses(List<Swap> swaps);
 }
---
+++ OneCoin/Server/src/main/java/OneCoin/Server/swap/service/SwapService.java
@@ -0,0 +1,38 @@
+package OneCoin.Server.swap.service;
+
+import OneCoin.Server.coin.service.CoinService;
+import OneCoin.Server.swap.repository.SwapRepository;
+import OneCoin.Server.upbit.repository.TickerRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+@Transactional
+@Service
+public class SwapService {
+    private final SwapRepository swapRepository;
+    private final TickerRepository tickerRepository;
+    private final CoinService coinService;
+
+    public SwapService(SwapRepository swapRepository, TickerRepository tickerRepository, CoinService coinService) {
+        this.swapRepository = swapRepository;
+        this.tickerRepository = tickerRepository;
+        this.coinService = coinService;
+    }
+
+    /**
+     *  코인 환율 계산
+     */
+    private BigDecimal calculateExchangeRate(String givenCoinCode, String takenCoinCode, BigDecimal amount) {
+        // 코인 이름 체크
+        coinService.verifyCoinExists(givenCoinCode);
+        coinService.verifyCoinExists(takenCoinCode);
+        
+        BigDecimal givenCoinPrice = new BigDecimal(tickerRepository.findTickerByCode(givenCoinCode).getTradePrice());
+        BigDecimal takenCoinPrice = new BigDecimal(tickerRepository.findTickerByCode(takenCoinCode).getTradePrice());
+
+        return givenCoinPrice.divide(takenCoinPrice, 15, RoundingMode.HALF_UP).multiply(amount);
+    }
+}
