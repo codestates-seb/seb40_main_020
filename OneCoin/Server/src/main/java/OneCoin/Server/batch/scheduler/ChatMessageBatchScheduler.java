fbdee681acdd2df790b55d269cd5897a178bb4e0
broden-horizon <rlghd1698@naver.com>
2022-12-02 18:37:28 +0900

    ✨feat: Sprig Batch를 이용해서 In-Memory -> RDB 저장

---
+++ OneCoin/Server/src/main/java/OneCoin/Server/batch/scheduler/ChatMessageBatchScheduler.java
@@ -0,0 +1,35 @@
+package OneCoin.Server.batch.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecutionException;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+
+@Component
+@RequiredArgsConstructor
+public class ChatMessageBatchScheduler {
+    private final Job job;
+    private final JobLauncher jobLauncher;
+
+    @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.DAYS)
+    public void executeJob() {
+        try {
+            jobLauncher.run(
+                    job,
+                    new JobParametersBuilder()
+                            .addString("datetime", LocalDateTime.now().toString())
+                            .toJobParameters()
+            );
+        } catch (JobExecutionException ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+}
