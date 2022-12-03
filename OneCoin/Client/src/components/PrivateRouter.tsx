1d7cc9c3f765f7e05b19b4a1224b92b6a7f39d04
MINDA01 <ekdud4639@gmail.com>
2022-12-03 15:29:10 +0900

    ✨feat :  파비콘 추가 및 alert 교체

--- OneCoin/Client/src/components/PrivateRouter.tsx
+++ OneCoin/Client/src/components/PrivateRouter.tsx
@@ -1,10 +1,22 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { Navigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 function PrivateRoute({ children }: { children: React.ReactNode }) {
 	const auth = localStorage.getItem('login-token');
 
-	if (!auth) alert('로그인 후 사용하실 수 있습니다.');
+	if (!auth)
+		Swal.fire({
+			title: '로그인 후 사용하실 수 있습니다.',
+			confirmButtonText: '확인',
+			showClass: {
+				popup: 'animate__animated animate__fadeInDown',
+			},
+			hideClass: {
+				popup: 'animate__animated animate__fadeOutUp',
+			},
+		});
+
 	// eslint-disable-next-line react/react-in-jsx-scope
 	return auth ? <>{children}</> : <Navigate to="/login"></Navigate>;
 }
