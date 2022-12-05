ba18d09761bfec74142d283a32e63b8e9c71370c
broden-horizon <rlghd1698@naver.com>
2022-12-05 22:01:25 +0900

    🧪Test: test용 레포 삭제 (배포 전 삭제 예정)

--- OneCoin/Server/src/main/java/OneCoin/Server/chat/controller/ChatRoomController.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/chat/controller/ChatRoomController.java
@@ -13,10 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -41,5 +38,9 @@ public class ChatRoomController {
         List<ChatMessage> messages = chatService.getChatMessages(chatRoomId, sessionId);
         List<ChatResponseDto> responses = chatMapper.chatMessagesToResponseDtos(messages);
         return new ResponseEntity<>(new MultiResponseDto<>(responses), HttpStatus.CREATED);
+    }
+    @DeleteMapping()
+    public void deleteInMemory() {
+        chatService.deleteInMemory();
     }
 }
